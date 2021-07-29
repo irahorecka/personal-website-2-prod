e34a4f7c6be6d15fb97e00ae66a20471106bed0c
Ira Horecka <ira89@icloud.com>
2021-07-29 14:26:20 -0700

    update email sync

--- scripts/mail/__init__.py
+++ scripts/mail/__init__.py
@@ -1,6 +1,9 @@
 import os
 from functools import partial
 
+from dotenv import load_dotenv
+
 from scripts.mail.mail import write_email
 
+load_dotenv()
 write_email = partial(write_email, os.environ["EMAIL_USER"], os.environ["EMAIL_PASS"])
