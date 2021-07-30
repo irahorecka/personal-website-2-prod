483b3652caa28b17cfc9b56152f9093c97eda8b1
Ira Horecka <ira89@icloud.com>
2021-07-29 17:31:28 -0700

    update crontab

--- cron.sh
+++ cron.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 crontab -l > mycron
+# Setup cron headers
+echo "SHELL=/bin/bash" >> mycron
 # For executing clean_craigslist_housing
-echo "0 6-22/2 * * * python update_db.py" >> mycron
+echo "0 6-22/2 * * * root source /home/irahorecka/pweb2/webenv/bin/activate && python /home/irahorecka/pweb2/update_db.py" >> mycron
 # For executing rm_expired_craigslist_housing
-echo "0 2 * * * python rm_expired_db.py" >> mycron
+echo "0 2 * * * root source /home/irahorecka/pweb2/webenv/bin/activate && python /home/irahorecka/pweb2/rm_expired_db.py" >> mycron
 crontab mycron
 rm mycron
