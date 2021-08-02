67217a4a1a926883128518248ec821fa66ef4ad5
Ira Horecka <ira89@icloud.com>
2021-08-01 22:07:30 -0700

    update search button

--- setup_db.py
+++ setup_db.py
@@ -15,10 +15,10 @@ from scripts.mail import write_email
 application = create_app()
 
 if __name__ == "__main__":
-    # Bonus: validate environment variable keys
+    # Bonus: validate environment variable keys.
     env_vars = ["EMAIL_USER", "EMAIL_PASS", "SECRET_KEY", "SQLALCHEMY_DATABASE_URI", "GITHUB_TOKEN"]
     for var in env_vars:
-        # Unprotected key calls - trigger exception if key doesn't exist
+        # Unprotected key calls - trigger exception if key doesn't exist.
         os.environ[var]
     try:
         setup(application)
