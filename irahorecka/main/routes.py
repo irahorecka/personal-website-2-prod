022e66495d1f68ef55cd927a33b2ac06f7a4424e
Ira Horecka <ira89@icloud.com>
2022-02-17 11:55:39 -0500

    add /humans.txt text file and route

--- irahorecka/main/routes.py
+++ irahorecka/main/routes.py
@@ -8,13 +8,14 @@ Flask blueprint to handle routes for irahorecka.com*.
 from pathlib import Path
 
 import yaml
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, Response
 
 from irahorecka.api import read_github_repos
 
 main = Blueprint("main", __name__)
 with open(Path(__file__).absolute().parent.parent.parent / "config.yaml", "r") as config:
     GITHUB_REPOS = yaml.safe_load(config)["github-repos"]
+TEMPLATES_MAIN_PATH = Path(__file__).absolute().parent.parent / "templates" / "main"
 
 
 @main.route("/")
@@ -36,3 +37,11 @@ def projects():
         "repos": read_github_repos(GITHUB_REPOS),
     }
     return render_template("main/projects.html", content=content)
+
+
+@main.route("/humans.txt")
+def humans_txt():
+    """humans.txt page of personal website."""
+    with open(TEMPLATES_MAIN_PATH / "humans.txt", "r") as f:
+        content = f.read()
+    return Response(content, content_type="text/plain")
