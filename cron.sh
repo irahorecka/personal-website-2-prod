006b21f61e0cf4fb904ca5fb56f9b790feb2b09f
Ira Horecka <ira89@icloud.com>
2022-03-15 14:48:47 -0400

    update cron.sh : 6hr --> 12hr per github request

--- cron.sh
+++ cron.sh
@@ -3,8 +3,8 @@
 crontab -l > mycron
 # Setup cron headers
 echo "SHELL=/bin/bash" >> mycron
-# For executing update_db.py every six hours everyday.
-echo "0 */6 * * * source /home/irahorecka/pweb2/webenv/bin/activate && python /home/irahorecka/pweb2/update_db.py" >> mycron
+# For executing update_db.py every twelve hours everyday.
+echo "0 */12 * * * source /home/irahorecka/pweb2/webenv/bin/activate && python /home/irahorecka/pweb2/update_db.py" >> mycron
 # For executing rm_expired_db.py at 02:00 everyday.
 # echo "0 2 * * * source /home/irahorecka/pweb2/webenv/bin/activate && python /home/irahorecka/pweb2/rm_expired_db.py" >> mycron
 crontab mycron
